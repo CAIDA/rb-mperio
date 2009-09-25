2946ab0138c0803b45e05d4447a4ac33acfa3c76
Young Hyun <youngh@caida.org>
2009-09-25 15:43:33 -0700

    Checking in work-in-progress: made progress in decoding mper responses.

--- ext/mp-mperio.c
+++ ext/mp-mperio.c
@@ -79,7 +79,9 @@
 #include "mp-mperio.h"
 
 typedef struct {
-  FILE *log;  /* may be NULL if the user didn't request logging */
+  VALUE delegate;  /* cache of @delegate for speed */
+  FILE *log;       /* may be NULL if the user didn't request logging */
+  int mper_error;  /* whether there was an error communicating with mper */
   int mper_fd;
   scamper_fd_t *mper_fdn;  /* wraps mper_fd */
   scamper_linepoll_t *lp;
@@ -90,20 +92,27 @@ typedef struct {
 
 static VALUE cMperIO;
 
-/*=========================================================================*/
+static ID iv_delegate_type;
+static ID meth_mperio_on_more, meth_mperio_on_data;
+static ID meth_mperio_on_error, meth_mperio_on_send_error;
+static ID meth_mperio_service_failure;
 
-static void send_command(mperio_data_t *data, const char *message)
-{
-  /* XXX somewhat inefficient to do a separate send for just the newline */
-  scamper_writebuf_send(data->wb, message, strlen(message));
-  scamper_writebuf_send(data->wb, "\n", 1);
-}
-
+static int connect_to_mper(int port, int use_tcp);
+static void handle_mper_ping_response(mperio_data_t *data,
+				      const control_word_t *resp_words,
+				      size_tword_count);
+static void send_command(mperio_data_t *data, const char *message);
+static void report_error(const char *msg_start, uint32_t reqnum,
+			 const char *msg_end);
+static void report_send_error(const char *msg_start, uint32_t reqnum,
+			      const char *msg_end);
+static VALUE create_error_message(const char *msg_start, const char *msg_end);
 
 /*=========================================================================*/
 
 /* Read callback for data->mper_fdn. */
-static void mperio_read_cb(int fd, void *param)
+static void
+mperio_read_cb(int fd, void *param)
 {
   mperio_data_t *data = (mperio_data_t *)param;
   ssize_t n;
@@ -113,9 +122,22 @@ static void mperio_read_cb(int fd, void *param)
   n = read(fd, data->read_buf, sizeof(data->read_buf));
   if (n < 0) {
     if (errno != EAGAIN && errno != EINTR) {
-      /* XXX call client's error callback */
+      int errno_saved = errno;
+#ifndef NDEBUG
       printerror(errno, strerror, __func__, "read failed");
+#endif
       scamper_fd_read_pause(data->mper_fdn);
+      data->mper_error = 1;
+
+      if (NIL_P(data->delegate)) {
+	rb_raise(rb_eRuntimeError, "no delegate set");
+      }
+      else {
+	char buf[256];
+	snprintf(buf, sizeof(buf), "error reading from mper: %s",
+		 strerror(errno_saved));
+	rb_funcall(data->delegate, meth_service_failure, 1, rb_str_new2(buf));
+      }
     }
   }
   else if (n > 0) {
@@ -123,29 +145,92 @@ static void mperio_read_cb(int fd, void *param)
   }
   else {  /* n == 0 */
     scamper_fd_read_pause(data->mper_fdn);
+    /* XXX handle EOF */
   }
 }
 
 
 /* Callback for data->lp.  Receives a single line per call. */
-static int mperio_read_line_cb(void *param, uint8_t *buf, size_t len)
+static int
+mperio_read_line_cb(void *param, uint8_t *buf, size_t len)
 {
   mperio_data_t *data = (mperio_data_t *)param;
+  const control_word_t *resp_words = NULL;
+  size_t word_count = 0;
+  char error_msg[128];
 
-  /* XXX call client callback */
+  if (NIL_P(data->delegate)) {
+    rb_raise(rb_eRuntimeError, "no delegate set");
+  }
+ 
+  if (strcmp((char *)buf, "MORE") == 0) {
+    rb_funcall(data->delegate, meth_on_more, 0);
+  }
+  else {
+    resp_words = parse_control_message((char *)buf, &word_count);
+    if (word_count == 0) {
+      report_error("couldn't parse mper response", resp_words[0].cw_uint,
+		   resp_words[1].cw_str);
+    }
+    else {
+      switch (words[1].cw_code) {
+      case KC_CMD_ERROR_CMD:
+	report_error("mper couldn't process our command",
+		     resp_words[0].cw_uint, resp_words[1].cw_str);
+	break;
+
+      case KC_SEND_ERROR_CMD:
+	report_send_error("send error", resp_words[0].cw_uint,
+			  resp_words[1].cw_str);
+	break;
+
+      case KC_RESP_TIMEOUT_CMD:
+      case KC_PING_RESP_CMD:
+	handle_mper_ping_response(data, resp_words, word_count);
+	break;
+
+      default:
+	snprintf(error_msg, sizeof(error_msg),
+		 "INTERNAL ERROR: unexpected response code %d from mper",
+		 words[1].cw_code);
+	report_error(error_msg, resp_words[0].cw_uint, (char *)buf);
+	break;
+      }
+    }
+  }
 
   return 0;  /* the return value isn't used in any way */
 }
 
 
+static void
+handle_mper_ping_response(mperio_data_t *data,
+			  const control_word_t *resp_words, size_tword_count)
+{
+    rb_funcall(data->delegate, meth_service_failure, 1, rb_str_new2(buf));
+}
+
+
 /* Callback for data->wb: error while writing to data->mper_fdn. */
-static void mperio_write_error_cb(void *param, int err, scamper_writebuf_t *wb)
+static void
+mperio_write_error_cb(void *param, int err, scamper_writebuf_t *wb)
 {
   mperio_data_t *data = (mperio_data_t *)param;
 
-  /* XXX call client's error callback */
+  data->mper_error = 1;
 
+#ifndef NDEBUG
   printerror(err, strerror, __func__, "write failed");
+#endif
+
+  if (NIL_P(data->delegate)) {
+    rb_raise(rb_eRuntimeError, "no delegate set");
+  }
+  else {
+    char buf[256];
+    snprintf(buf, sizeof(buf), "error writing to mper: %s", strerror(err));
+    rb_funcall(data->delegate, meth_service_failure, 1, rb_str_new2(buf));
+  }
 }
 
 
@@ -157,7 +242,8 @@ static void mperio_write_error_cb(void *param, int err, scamper_writebuf_t *wb)
  * and it can be cleaned up
  */
 /* Callback for data->wb: all queued data written out to data->mper_fdn. */
-static void mperio_write_drained_cb(void *param, scamper_writebuf_t *wb)
+static void
+mperio_write_drained_cb(void *param, scamper_writebuf_t *wb)
 {
   mperio_data_t *data = (mperio_data_t *)param;
 
@@ -167,7 +253,8 @@ static void mperio_write_drained_cb(void *param, scamper_writebuf_t *wb)
 
 /*=========================================================================*/
 
-static void mperio_free(void *data)
+static void
+mperio_free(void *data)
 {
   mperio_data_t *mperio_data = (mperio_data_t *)data;
 
@@ -182,13 +269,67 @@ static void mperio_free(void *data)
   }
 }
 
-static VALUE mperio_alloc(VALUE klass)
+static VALUE
+mperio_alloc(VALUE klass)
 {
   return Data_Wrap_Struct(klass, 0, mperio_free, 0);
 }
 
 
-static int connect_to_mper(int port, int use_tcp)
+static VALUE
+mperio_init(int argc, VALUE *argv, VALUE self)
+{
+  mperio_data_t *data = NULL;
+  VALUE vport, vlog_path, vuse_tcp;
+  int port, fd;
+  FILE *log = NULL;
+
+  rb_scan_args(argc, argv, "12", &vport, &vlog_path, &vuse_tcp);
+  port = NUM2INT(vport);
+
+  if (!NIL_P(vlog_path)) {
+    SafeStringValue(vlog_path);
+    if ((log = fopen(RSTRING_PTR(vlog_path), "a")) == NULL) {
+      rb_sys_fail("could not open MperIO log file");
+    }
+  }
+
+  fd = connect_to_mper(port, RTEST(vuse_tcp));
+  assert(fd != -1);
+
+  data = ALLOC(mperio_data_t);
+  memset(data, 0, sizeof(mperio_data_t));
+  data->delegate = Qnil;
+  data->log = log;
+  data->mper_fd = fd;
+  DATA_PTR(self) = data;
+
+  data->mper_fdn = scamper_fd_private(fd, NULL, NULL, NULL, NULL);
+  if (data->mper_fdn) {
+    data->lp = scamper_linepoll_alloc(mperio_read_line_cb, data);
+    if (data->lp) {
+      data->wb = scamper_writebuf_alloc();
+      if (data->wb) {
+	scamper_writebuf_attach(data->wb, data->mper_fdn, data,
+				mperio_write_error_cb,
+				mperio_write_drained_cb);
+	return self;
+      }
+      scamper_linepoll_free(data->lp, 0);
+    }
+    scamper_fd_free(data->mper_fdn);  /* doesn't close data->mper_fd */
+  }
+
+  if (data->log) fclose(data->log);
+  close(data->mper_fd);
+  free(data);
+  DATA_PTR(self) = NULL;
+  return Qnil;
+}
+
+
+static int
+connect_to_mper(int port, int use_tcp)
 {
   struct sockaddr *sa = NULL;
   socklen_t sa_len;
@@ -257,73 +398,36 @@ static int connect_to_mper(int port, int use_tcp)
 }
 
 
-static VALUE mperio_init(int argc, VALUE *argv, VALUE self)
+static VALUE
+mperio_set_delegate(VALUE self, VALUE delegate)
 {
   mperio_data_t *data = NULL;
-  VALUE vport, vlog_path, vuse_tcp;
-  int port, fd;
-  FILE *log = NULL;
 
-  rb_scan_args(argc, argv, "12", &vport, &vlog_path, &vuse_tcp);
-  port = NUM2INT(vport);
+  /* Set the instance variable so that the delegate is retained by the GC. */
+  rb_ivar_set(self, iv_delegate, delegate);
 
-  if (!NIL_P(vlog_path)) {
-    SafeStringValue(vlog_path);
-    if ((log = fopen(RSTRING_PTR(vlog_path), "a")) == NULL) {
-      rb_sys_fail("could not open MperIO log file");
-    }
-  }
-
-  fd = connect_to_mper(port, RTEST(vuse_tcp));
-  assert(fd != -1);
-
-  data = ALLOC(mperio_data_t);
-  memset(data, 0, sizeof(mperio_data_t));
-  data->log = log;
-  data->mper_fd = fd;
-  DATA_PTR(self) = data;
-
-  data->mper_fdn = scamper_fd_private(fd, NULL, NULL, NULL, NULL);
-  if (data->mper_fdn) {
-    data->lp = scamper_linepoll_alloc(mperio_read_line_cb, data);
-    if (data->lp) {
-      data->wb = scamper_writebuf_alloc();
-      if (data->wb) {
-	scamper_writebuf_attach(data->wb, data->mper_fdn, data,
-				mperio_write_error_cb,
-				mperio_write_drained_cb);
-	return self;
-      }
-      scamper_linepoll_free(data->lp, 0);
-    }
-    scamper_fd_free(data->mper_fdn);  /* doesn't close data->mper_fd */
-  }
-
-  if (data->log) fclose(data->log);
-  close(data->mper_fd);
-  free(data);
-  DATA_PTR(self) = NULL;
-  return Qnil;
+  Data_Get_Struct(self, mperio_data_t, data);
+  data->delegate = delegate;  /* for performance */
+  return self;
 }
 
 
-static VALUE mperio_set_delegate(VALUE self, VALUE delegate)
+static VALUE
+mperio_start(VALUE self)
 {
-  /* XXX */
+  mperio_data_t *data = NULL;
+
+  Data_Get_Struct(self, mperio_data_t, data);
+  if (NIL_P(data->delegate)) {
+    rb_raise(rb_eRuntimeError, "no delegate set");
+  }
 
   return self;
 }
 
 
-static VALUE mperio_start(VALUE self)
-{
-  /* XXX */
-
-  return self;
-}
-
-
-static VALUE mperio_ping_icmp(VALUE self, VALUE vreqnum, VALUE vdest)
+static VALUE
+mperio_ping_icmp(VALUE self, VALUE vreqnum, VALUE vdest)
 {
   mperio_data_t *data = NULL;
   uint32_t reqnum;
@@ -349,8 +453,9 @@ static VALUE mperio_ping_icmp(VALUE self, VALUE vreqnum, VALUE vdest)
 }
 
 
-static VALUE mperio_ping_icmp_indir(VALUE self, VALUE vreqnum, VALUE vdest,
-				    VALUE vhop, VALUE vcksum)
+static VALUE
+mperio_ping_icmp_indir(VALUE self, VALUE vreqnum, VALUE vdest,
+		       VALUE vhop, VALUE vcksum)
 {
   mperio_data_t *data = NULL;
   uint32_t reqnum, hop, cksum;
@@ -380,7 +485,8 @@ static VALUE mperio_ping_icmp_indir(VALUE self, VALUE vreqnum, VALUE vdest,
 }
 
 
-static VALUE mperio_ping_tcp(VALUE self, VALUE vreqnum, VALUE vdest,VALUE vdport)
+static VALUE
+mperio_ping_tcp(VALUE self, VALUE vreqnum, VALUE vdest,VALUE vdport)
 {
   mperio_data_t *data = NULL;
   uint32_t reqnum, dport;
@@ -408,13 +514,65 @@ static VALUE mperio_ping_tcp(VALUE self, VALUE vreqnum, VALUE vdest,VALUE vdport
 }
 
 
+static void
+send_command(mperio_data_t *data, const char *message)
+{
+  /* XXX somewhat inefficient to do a separate send for just the newline */
+  scamper_writebuf_send(data->wb, message, strlen(message));
+  scamper_writebuf_send(data->wb, "\n", 1);
+}
+
+
+static void
+report_error(const char *msg_start, uint32_t reqnum, const char *msg_end)
+{
+  VALUE msg = create_error_message(msg_start, msg_end);
+  rb_funcall(data->delegate, meth_on_error, 2, ULONG2NUM(reqnum), msg);
+}
+
+
+static void
+report_send_error(const char *msg_start, uint32_t reqnum, const char *msg_end)
+{
+  VALUE msg = create_error_message(msg_start, msg_end);
+  rb_funcall(data->delegate, meth_on_send_error, 2, ULONG2NUM(reqnum), msg);
+}
+
+
+static VALUE
+create_error_message(const char *msg_start, const char *msg_end)
+{
+  size_t len = strlen(msg_start) + 2 + strlen(msg_end);
+  char *buf;
+  VALUE retval;
+
+  buf = ALLOC_N(char, len + 1);
+  strcpy(buf, msg_start);
+  strcat(buf, ": ");
+  strcat(buf, msg_end);
+
+  retval = rb_str_new2(buf);
+  free(buf);
+
+  return retval;
+}
+
+
 /***************************************************************************/
 /***************************************************************************/
 
-void Init_mp_mperio(void)
+void
+Init_mp_mperio(void)
 {
   ID private_class_method_ID, private_ID;
   ID /*new_ID,*/ dup_ID, clone_ID;
+
+  iv_delegate = rb_intern("@delegate");
+  meth_mperio_on_more = rb_intern("mperio_on_more");
+  meth_mperio_on_data = rb_intern("mperio_on_data");
+  meth_mperio_on_error = rb_intern("mperio_on_error");
+  meth_mperio_on_send_error = rb_intern("mperio_on_send_error");
+  meth_mperio_service_failure = rb_intern("mperio_service_failure");
 
   /* XXX make MperIO a singleton */
   /* XXX fix message creation/parsing routines to not use static buffers */
